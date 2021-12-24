9cb0164f3726840e3f96e17afdbcee2fd9cc65ed
Michael <MichaelCheung101@gmail.com>
2021-12-24 18:04:21 -0500

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+const writeCards = (name) => {
+    let result = []
+    for(let i = 0 ; i < name.length; i++){ 
+        console.log(`Thank you, ${name[i]}, for the wonderful surprise gift!`)
+        result.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`)    
+    }
+    return result
+}
+
+const countDown = () => {
+    let countup = 0
+    while (countup < 11){
+        console.log(countup++)
+   } 
+}
